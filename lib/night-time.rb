3072ad851318bce83e96125413393302c84ab67d
maiha <maiha@wota.jp>
2010-01-11 01:28:33 +0900

    Implemented night-time

--- lib/night-time.rb
+++ lib/night-time.rb
@@ -1,9 +1,32 @@
 require 'parsedate'
 
-module NightTime
-  def parse(text)
-    raise NotImplementedError
+class Time
+  class << self
+    def mod_midnight(hour, min, sec)
+      extra = 0
+
+      squeeze = lambda{|val, max, unit, extra|
+        num, new_val = val.divmod(max)
+        [new_val, extra + max*num*unit]
+      }
+      sec , extra = squeeze.call(sec.to_i , 60, 1    , extra)
+      min , extra = squeeze.call(min.to_i , 60, 60   , extra)
+      hour, extra = squeeze.call(hour.to_i, 24, 60*60, extra)
+
+      return [hour, min, sec, extra]
+    end
+
+
+    alias mktime_without_night_time mktime
+
+    def mktime(*args)
+      year, mon, day, hour, min, sec, usec = args
+      hour, min, sec, extra = mod_midnight(hour, min, sec)
+      mktime_without_night_time(year, mon, day, hour, min, sec, usec) + extra
+    end
+
+    def parse(text)
+      Time.mktime(*ParseDate.parsedate(text)[0,7])
+    end
   end
 end
-
-Time.extend NightTime
